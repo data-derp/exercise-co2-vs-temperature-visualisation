60301d523bd6b2ab0da0c4f5006cbc897b1b7a3f
Kelsey Mok <kelseymok@gmail.com>
2022-07-02 16:58:42 +0200

    removing all traces of aws

--- Data Visualisation CO2 vs. Temperature.py
+++ Data Visualisation CO2 vs. Temperature.py
@@ -10,32 +10,119 @@
 
 # COMMAND ----------
 
-!pip install pandas==1.2 s3fs plotly scikit-learn==0.24.2 dash
+# MAGIC %pip install pandas==1.2 s3fs plotly scikit-learn==0.24.2 dash wget
 
+# COMMAND ----------
+
+# Clear out existing working directory
+
+current_user=dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get().split("@")[0]
+working_directory=f"/FileStore/{current_user}/dataVisualisation"
+dbutils.fs.rm(working_directory, True)
+
+# COMMAND ----------
+
+# Function that checks that files exist from the previous exercise
+
+def contains_expected_files(dir, expected_files):
+    match_expected_extensions=lambda x: x.endswith(tuple(expected_files))
+    get_name=lambda x: x.name
+    try: 
+        num_matching_files = len(list(filter(match_expected_extensions, map(get_name, dbutils.fs.ls(dir)))))
+        return num_matching_files >= len(expected_ingestion_parquet_files)
+    except Exception as e:
+        if 'java.io.FileNotFoundException' in str(e):
+            return False
+        else:
+            raise
+
+# COMMAND ----------
+
+# Function to download files to DBFS
+
+import os
+import wget
+import sys
+import shutil
+
+sys.stdout.fileno = lambda: False # prevents AttributeError: 'ConsoleBuffer' object has no attribute 'fileno'   
+
+def clean_remake_dir(dir):
+    if os.path.isdir(local_tmp_dir): shutil.rmtree(local_tmp_dir)
+    os.makedirs(local_tmp_dir)
+    
+
+def download_to_local_dir(local_dir, target_dir, url, filename_parsing_lambda):
+    filename = (filename_parsing_lambda)(url)
+    tmp_path = f"{local_dir}/{filename}"
+    target_path = f"{target_dir}/{filename}"
+    if os.path.exists(tmp_path):
+        os.remove(tmp_path) 
+    
+    saved_filename = wget.download(url, out = tmp_path)
+    
+    if target_path.endswith(".zip"):
+        with zipfile.ZipFile(tmp_path, 'r') as zip_ref:
+            zip_ref.extractall(local_dir)
+
+    dbutils.fs.cp(f"file:{local_dir}/", target_dir, True)
+    
+    return target_path
+
+# COMMAND ----------
+
+# If prerequisite files don't already exist, download them
+
+import zipfile
+
+
+expected_ingestion_parquet_dirs = [
+    f"/FileStore/{current_user}/dataTransformation/CountryEmissionsVsTemperatures.parquet/",
+    f"/FileStore/{current_user}/dataTransformation/EuropeBigThreeEmissions.parquet/",
+    f"/FileStore/{current_user}/dataTransformation/GlobalEmissionsVsTemperatures.parquet/",
+    f"/FileStore/{current_user}/dataTransformation/OceaniaEmissionsEdited.parquet/"
+]
+
+expected_ingestion_parquet_files = [
+    "_SUCCESS",
+    ".parquet"
+]
+
+expected_files_exist = all(list(map(lambda x: contains_expected_files(x, expected_ingestion_parquet_files), expected_ingestion_parquet_dirs)))
+
+source_directory = f"/FileStore/{current_user}/dataTransformation"
+
+if expected_files_exist:
+    print("Prerequisite files exist. Nothing to do here!")
+else:
+    print("Prerequisite files don't yet exist. Downloading...")
+    
+    local_tmp_dir = f"{os.getcwd()}/{current_user}/dataTransformation/tmp"
+    clean_remake_dir(local_tmp_dir)
+
+    url = "https://github.com/data-derp/exercise-co2-vs-temperature-visualisation/blob/master/transformation-outputs/transformation-outputs.zip?raw=true"
+    
+    download_to_local_dir(local_tmp_dir, source_directory, url, lambda y: y.split("/")[-1].replace("?raw=true",""))
+    
+    
+display(spark.createDataFrame(dbutils.fs.ls(source_directory)))
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC 
-# MAGIC ## Read Parquet Data from S3
-# MAGIC Let's read our data from S3. Remember, that typically, there would be some access layer in between you and the data. Right now, we're reading non-sensitive data from an S3 bucket (see `storage_options`) and [other examples](https://docs.dask.org/en/stable/how-to/connect-to-remote-data.html). Don't forget to replace `"YOUR-BUCKET-NAME"` with the bucket that contains your data.
+# MAGIC ## Read Parquet Data from our Data Transformation Output
+# MAGIC Let's read our data from our Data Transformation output. Note: behind the scenes, this exists in AWS S3 blob storage. Remember, that typically, there would be some access layer in between you and the data. Right now, we're reading non-sensitive data from our DBFS storage which is in an S3 bucket (see `storage_options`) and [other examples](https://docs.dask.org/en/stable/how-to/connect-to-remote-data.html).
 
 # COMMAND ----------
 
-import pandas as pd
-
-bucket_name="YOUR-BUCKET_NAME" # TODO: Change me!
-country_df = pd.read_parquet("s3://" + bucket_name + "/data-transformation/CountryEmissionsVsTemperatures.parquet/", storage_options={'anon': True, 'use_ssl': False})
-display(country_df)
+europe_big_three_emissions_df = spark.read.parquet(f"{source_directory}/EuropeBigThreeEmissions.parquet/")
+display(europe_big_three_emissions_df)
 
 # COMMAND ----------
 
 ## If you would like to experiment with Machine Learning
 import sklearn
-
-# COMMAND ----------
-
-country_df['Country'].unique()
 
 # COMMAND ----------
 
--- README.md
+++ README.md
@@ -1,82 +1,20 @@
 # CO2 vs. Temperature (Visualisation)
-In this exercise, we assume that you've completed the [production-code](https://github.com/data-derp/exercise-co2-vs-temperature-production-code) and have managed to push those artifacts to an AWS S3 Bucket. 
-
-If you don't already hav ethese artifacts, follow the instructions under [Fresh Start](#fresh-start).
-
-**NOTE:** The following exercises follow the same concept as [the production-code exercise](https://github.com/data-derp/exercise-co2-vs-temperature-production-code) where a `project-name` and `module-name` are used consistently to create resources.
-
-In all examples in ,
-* `project-name` = **awesome-project**
-* `module-name` = **awesome-module**
-
-Where these are used, you'll want to pick your own unique `project-name` and `module-name`.
-
-## Prerequisites
-* An AWS Bucket containing project data (see [Fresh Start](#fresh-start))
+In this exercise, we assume that you've completed the Data Ingestion and Data Transformation exercises.
 
 ## Quickstart
-1. Ensure your AWS bucket containing the relevant project data exists (if it doesn't or if you have no idea what this is, please see [Fresh Start](#fresh-start))
-2. Set up a [Databricks Account](https://github.com/data-derp/documentation/blob/master/databricks/README.md) if you don't already have one
-3. [Create a cluster](https://github.com/data-derp/documentation/blob/master/databricks/setup-cluster.md) if you don't already have one
-4. [Make your bucket public](#make-your-bucket-public)
+1. Set up a [Databricks Account](https://github.com/data-derp/documentation/blob/master/databricks/README.md) if you don't already have one
+2. [Create a cluster](https://github.com/data-derp/documentation/blob/master/databricks/setup-cluster.md) if you don't already have one
 
-5. In your User's workspace, click import
+3. In your User's workspace, click import
 
    ![databricks-import](https://github.com/data-derp/documentation/blob/master/databricks/assets/databricks-import.png?raw=true)
 
-6. Import the `Data Visualisation CO2 vs Temperature.py` notebook using the URL method: `https://github.com/data-derp/exercise-co2-vs-temperature-visualisation/blob/master/Data%20Visualisation%20CO2%20vs.%20Temperature.py`
+4. Import the `Data Visualisation CO2 vs Temperature.py` notebook using the URL method: `https://github.com/data-derp/exercise-co2-vs-temperature-visualisation/blob/master/Data%20Visualisation%20CO2%20vs.%20Temperature.py`
 
    ![databricks-import-url](https://github.com/data-derp/documentation/blob/master/databricks/assets/databricks-import-url.png?raw=true)
 
-7. Select your cluster
+5. Select your cluster
 
    ![databricks-select-cluster.png](https://github.com/data-derp/documentation/blob/master/databricks/assets/databricks-select-cluster.png?raw=true)
 
-8. Follow instructions
-
-
-## Fresh Start
-If you don't have the artifacts in an S3 bucket yet:
-1. [Ensure you have an active AWS CLI Session in your Terminal](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html))
-2. [Create an S3 bucket](https://github.com/data-derp/s3-bucket-aws-cloudformation)
-3. Upload the transformed data:
-```bash
-# Change these variables
-PROJECT_NAME=awesome-project
-MODULE_NAME=awesome-module
-
-./go upload-data "${PROJECT_NAME}-${MODULE_NAME}"
-```
-
-## Make Your Bucket Public
-In most scenarios, your bucket containing data should NOT be public (open to the entire internet) unless your use case deems it appropriate.
-
-1. In the **AWS Console**, navigate to your bucket and click the **Permissions** tab.
-2. Click **Edit** under **Block Public Access**
-    ![block-public-access-edit.png](./assets/block-public-access-edit.png)
-3. Uncheck everything
-   ![block-public-access-uncheck.png](./assets/block-public-access-uncheck.png)
-4. Under the **Bucket Policy**, click **Edit** and paste the following policy (don't forget to change <YOUR BUCKET NAME>):
-```json
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AllowPublicRead",
-            "Principal": "*",
-            "Effect": "Allow",
-            "Action": ["s3:GetObject"],
-            "Resource": ["arn:aws:s3:::<YOUR BUCKET NAME>/*"]
-        }
-   ]
-}
-```
-5. Under the **Access Control List (ACL)**:
-
-   ![acl.png](./assets/acl.png)
-
-   ![acl-everyone-list-objects.png](./assets/acl-everyone-list-objects.png)
-
-   ![acl-agree-public.png](./assets/acl-agree-public.png)
-    
-
+6. Follow instructions
